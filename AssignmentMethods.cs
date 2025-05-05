b7655ea19c350e2736e55c94e9e50507627f75f7
Soomin Song <song.soomin.418@outlook.com>
2025-05-05 14:49:11 -0700

    Add project files.

---
+++ AssignmentMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5._2
+{
+    internal class AssignmentMethods
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            string[] words = s.Trim().Split(' ');
+            Console.WriteLine(s);
+            Console.WriteLine($"The last word is '{words[words.Length-1]}' with Length {words[words.Length - 1].Length}");
+            return words[words.Length - 1].Length;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Assignment_5._2;
+
+mainMenu();
+
+
+void mainMenu()
+{
+    char choice;
+    do
+    {
+        Console.WriteLine("Select Assignment");
+        Console.WriteLine("1: 5.2.1");
+        Console.WriteLine("2: 5.2.2");
+        Console.WriteLine("3: 5.2.3");
+        Console.WriteLine("4: 5.2.4");
+        Console.WriteLine("q: quit");
+        choice = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+        switch (choice)
+        {
+            case '1':
+                Console.Clear();
+                Console.WriteLine("1. Given a string s consisting of words and spaces, return the length of the last word in the string. A word is a maximal substring consisting of non-space characters only.\n");
+                AssignmentMethods.LengthOfLastWord("Hello World");
+                AssignmentMethods.LengthOfLastWord(" fly me to the moon ");
+
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+        }
+        Console.WriteLine();
+    } while (choice != 'q');
+}
